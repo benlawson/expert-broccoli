5a2f776661e6c86a5639c856e47e0bd00efefd46
Ben L <balawson@bu.edu>
2017-11-20 17:16:19 -0500

    early classifiers and visualizations

--- plot_precision_recall_curve.py
+++ plot_precision_recall_curve.py
@@ -14,12 +14,14 @@ from sklearn.preprocessing import label_binarize
 
 from itertools import cycle
 # setup plot details
-colors = cycle(['navy', 'turquoise', 'darkorange', 'cornflowerblue', 'teal'])
-algos = ["XGBoost", "Decision Tree"]
+colors = cycle(['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:olive', 'tab:cyan'])
+
+algos = ["Nearest Neighbors", "SVM", "Decision Tree", "Random Forest", "Single Layer Perceptron", "AdaBoost", "Naive Bayes", "LDA", "XGBoost"]
+
+filenames = ["nearestneighbors", "svm", "decisiontree", "randomforest", "neuralnet", "adaboost", "naivebayes", "lda", "xgb"]
 
 n_classes = 11
 X_train, X_test, y_train, y_test = joblib.load("testdata.joblib")
-
 
 
 # load classifier
@@ -32,11 +34,13 @@ def calculate_p_r(y_test, y_predict):
 
 precision, recall, average_precision = [], [], []
 
-for model_filename in ["xgb.joblib", 'decisiontree.joblib']:
-    model = joblib.load(model_filename)
+for model_filename in filenames:
+    model = joblib.load("{0}.joblib".format(model_filename))
     y_predict = model.predict(X_test)
     if type(y_predict[0]) == int or type(y_predict[0]) == np.int64:
         y_predict = label_binarize(y_predict, range(n_classes))
+    if type(y_test[0]) == int or type(y_test[0]) == np.int64:
+        y_test= label_binarize(y_test, range(n_classes))
     p, r, ap  = calculate_p_r(y_test, y_predict)
     precision.append(p)
     recall.append(r)
@@ -54,10 +58,10 @@ for f_score in f_scores:
     x = np.linspace(0.01, 1)
     y = f_score * x / (2 * x - f_score)
     l, = plt.plot(x[y >= 0], y[y >= 0], color='gray', alpha=0.2)
-    plt.annotate('f1={0:0.1f}'.format(f_score), xy=(0.9, y[45] + 0.02))
+    plt.annotate('$f_1={0:0.1f}$'.format(f_score), xy=(0.9, y[45] + 0.02))
 
 lines.append(l)
-labels.append('iso-f1 curves')
+labels.append('$iso-f_1 curves$')
 
 
 # plot each algorithm
@@ -69,13 +73,12 @@ for i, (color, algo) in enumerate(zip(colors, algos)):
 
 
 fig = plt.gcf()
-fig.subplots_adjust(bottom=0.25)
+#fig.subplots_adjust(right=0.25)
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('Recall')
 plt.ylabel('Precision')
-plt.title('Precision-Recall curves for some approaches')
-plt.legend(lines, labels, loc=(0, -.38), prop=dict(size=14))
+plt.title("Precision-Recall curves for some approaches based on PCA'd raw image data  representations")
+lgd = plt.legend(lines, labels, loc='upper left', prop=dict(size=14), bbox_to_anchor=(1.02, 1))
 
-
-plt.savefig('algos.svg')
+plt.savefig('algos_pca.png', bbox_extra_artists=(lgd,), bbox_inches="tight")
---
+++ predict_all.py
@@ -0,0 +1,75 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.preprocessing import label_binarize
+from xgboost import XGBClassifier
+import joblib
+from joblib import Parallel, delayed
+
+names = ["Nearest Neighbors", "SVM", "Decision Tree", "Random Forest", "Neural Net", "AdaBoost", "Naive Bayes", "LDA", "XGBoost"]
+
+filenames = ["nearestneighbors", "svm", "decisiontree", "randomforest", "neuralnet", "adaboost", "naivebayes", "lda", "xgb"]
+
+
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(kernel="linear", C=0.025),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    MLPClassifier(alpha=1),
+    AdaBoostClassifier(),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis(),
+    XGBClassifier()
+    ]
+
+data = joblib.load("representations.joblib")
+
+X = []
+labels = []
+for label, key in enumerate(data):
+    for _ in data[key][0]:
+        labels.append(label)
+    for x in data[key]:
+        X.extend(x)
+
+y = np.array(labels)
+X = np.array(X)
+
+X = joblib.load("./pca_representations.joblib")
+
+print(X.shape)
+print(y.shape)
+y_array = np.array(label_binarize(y, range(len(set(y)))))
+
+X_train, X_test, y_train, y_test =  train_test_split(X, y, random_state=42)
+X_train, X_test, y_train_array, y_test_array =  train_test_split(X, y_array, random_state=42)
+
+joblib.dump((X_train, X_test, y_train, y_test), "testdata.joblib")
+print(np.array(y_test).shape)
+print(np.array(y_test_array).shape)
+
+def train_save(model, filename):
+    try:
+        model.fit(X_train, y_train)
+    except ValueError:
+        model.fit(X_train, y_train_array)
+    joblib.dump(model, "{}.joblib".format(filename))
+    try:
+        print("{0}: {1} acc".format(filename, model.score(X_test, y_test)))
+    except ValueError:
+        print("{0}: {1} acc".format(filename, model.score(X_test, y_test_array)))
+
+
+Parallel(n_jobs=-1)(delayed(train_save)(m, f) for (m,f) in zip(classifiers, filenames))
+
+#for model, filename in zip(classifiers, filenames):
+#    model.fit(X_train, y_train)
+#    joblib.dump(model, "{}.joblib".format(filename))
+
