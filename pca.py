ad008ade5ade9df828543d959a6fc5d9149489ad
Ben L <balawson@bu.edu>
2017-11-21 20:37:31 -0500

    new file paths and stuff

--- pca.py
+++ pca.py
@@ -3,16 +3,8 @@ import numpy as np
 from sklearn.decomposition import PCA
 
 
-data = joblib.load('./images.joblib')
-
-X = []
-labels = []
-for label, key in enumerate(data):
-    for _ in data[key][0]:
-        labels.append(label)
-    for x in data[key]:
-        X.append(x)
-
+data = joblib.load('./preprocessed_imgs.joblib')
+X = np.array(data[0]).reshape(-1, 299, 299, 3)
 
 #greyscale images via averaging RGBs
 images = np.array(X).mean(axis=3)
@@ -22,5 +14,5 @@ images = images.reshape(963, -1)
 
 pca = PCA(n_components=2048)
 X_pca = pca.fit_transform(images)
-
+print(X_pca.shape)
 joblib.dump(X_pca, 'pca_representation.joblib')
