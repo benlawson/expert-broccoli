47aa1c60c8a88e7a4e274b153d2aeddee6999581
Ben L <balawson@bu.edu>
2017-12-07 12:21:57 -0500

    standardize dataset

---
+++ make_dataset_split.py
@@ -0,0 +1,37 @@
+#standard libraries
+import os
+import glob
+
+#external libraries
+import numpy as np
+import joblib
+from keras.applications.inception_v3 import InceptionV3 #, preprocess_input
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing.image import load_img, img_to_array
+from sklearn.preprocessing import LabelBinarizer
+
+X = []
+y = []
+
+ # filenames = sorted(glob.glob("/backupdrive/datasets/BUSampleDataSet/*.jpg"))
+for folder in sorted(glob.glob("../WinEarthPhotosByKeyword/*")):
+    print(folder)
+    filenames = sorted(glob.glob(os.path.join(folder, "*.jpg")))[:2]
+    for filename in filenames:
+        image = img_to_array(load_img(filename, target_size=(299,299)))
+        X.append(image)
+        y.append(os.path.basename(folder))
+
+inception_model = InceptionV3(include_top=False, pooling='avg')
+resnet_model    = ResNet50(include_top=False, pooling='avg')
+imgs = X
+inception_reps = inception_model.predict(np.array(X))
+resnet_reps    = resnet_model.predict(np.array((X)))
+encoder = LabelBinarizer()
+one_hot_encoded = encoder.fit_transform(y)
+
+joblib.dump((inception_reps), "inception_representations.joblib")
+joblib.dump((resnet_reps), "resnet_representations.joblib")
+joblib.dump((X), "preprocessed_imgs.joblib")
+joblib.dump((one_hot_encoded, y, encoder), "labels.joblib")
+
